ca2b0abb8d647aad55426d3711e21a818481ad98
twtysvn <ionutzmadalin@gmail.com>
2016-03-30 23:00:40 +0300

    edit text on new expense with auto open keyboard

--- app/src/main/java/com/apps27/dough/moneyclip/MainActivity.java
+++ app/src/main/java/com/apps27/dough/moneyclip/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                         */
-                Intent intent = new Intent(getApplicationContext(), NewRecordActivity.class);
+                Intent intent = new Intent(getApplicationContext(), RecordsNewActivity.class);
                 startActivity(intent);
             }
         });
